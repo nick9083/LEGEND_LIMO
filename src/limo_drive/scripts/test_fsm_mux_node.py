a5fe22d185cf381738c7fc58179668ce04680f77
nick9083 <userf9083@gmail.com>
2025-12-18 13:39:35 +0900

    add

--- src/limo_drive/scripts/test_fsm_mux_node.py
+++ src/limo_drive/scripts/test_fsm_mux_node.py
@@ -70,7 +70,7 @@ class FSMMuxNode:
         self.loop_rate = rospy.get_param("~loop_rate", 30.0)
 
         # ----- Mission3 타이밍 파라미터 -----
-        self.m3_phase_time          = rospy.get_param("~m3_phase_time", 13)   # 첫 LKAS cmd 이후 기준
+        self.m3_phase_time          = rospy.get_param("~m3_phase_time", 13.4)   # 첫 LKAS cmd 이후 기준
         self.m3_turn_duration       = rospy.get_param("~m3_turn_duration", 2.5)
         self.m3_turn_speed          = rospy.get_param("~m3_turn_speed", 0.18)
         self.m3_turn_yaw            = rospy.get_param("~m3_turn_yaw", -0.4)
@@ -78,7 +78,7 @@ class FSMMuxNode:
         # mission3 DWA 강제 구간
         self.m3_force_obs_duration  = rospy.get_param("~m3_force_obs_duration", 4.0)
         # mission3 끝난 직후 base obstacle 강제 구간
-        self.m3_force_base_duration = rospy.get_param("~m3_force_base_duration", 0.1)
+        self.m3_force_base_duration = rospy.get_param("~m3_force_base_duration", 4.0)
 
         # ===== v2x 두 단계 타이머 =====
         # mission5: 첫 번째 v2x 턴이 시작되는 시점
--- src/limo_drive/scripts/test_obstacle_avoid_node.py
+++ src/limo_drive/scripts/test_obstacle_avoid_node.py
@@ -215,7 +215,7 @@ class Limo_obstacle_avoidence:
         # ===== gap 기반 주행 파라미터 =====
         self.free_dist      = rospy.get_param("~free_dist", 0.6)
         self.fov_deg        = rospy.get_param("~fov_deg", 20.0)
-        self.linear_speed   = rospy.get_param("~linear_speed", 0.4)
+        self.linear_speed   = rospy.get_param("~linear_speed", 0.5)
         self.k_ang          = rospy.get_param("~k_ang", 1.0)
         self.max_yaw        = rospy.get_param("~max_yaw", 1.0)
         self.min_gap_width_m = rospy.get_param("~min_gap_width_m", 0.20)
