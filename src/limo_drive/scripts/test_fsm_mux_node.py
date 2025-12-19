04248530076e9df8d62a42a7caa0144d96633dbd
nick9083 <userf9083@gmail.com>
2025-12-19 11:46:23 +0900

    add_

--- src/limo_drive/scripts/test_fsm_mux_node.py
+++ src/limo_drive/scripts/test_fsm_mux_node.py
@@ -88,7 +88,7 @@ class FSMMuxNode:
 
         # 두 턴에 *각각* 쓰는 턴 지속 시간
         self.mission5_turn_duration = rospy.get_param("~mission5_turn_duration", 0)
-        self.v2x_turn_duration      = rospy.get_param("~v2x_turn_duration", 1)
+        self.v2x_turn_duration      = rospy.get_param("~v2x_turn_duration", 1.6)
 
         # mission5(1단계 턴) 조향값
         self.mission5_turn_speed = rospy.get_param("~mission5_turn_speed", 0.0)
