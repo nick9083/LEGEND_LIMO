d36a47bbfed1ce2fc324d2b2a8b0f6f25f0376d5
nick9083 <userf9083@gmail.com>
2025-12-19 11:36:48 +0900

    add

--- src/limo_drive/scripts/robust_fsm_mux_node.py
+++ src/limo_drive/scripts/robust_fsm_mux_node.py
@@ -70,33 +70,33 @@ class FSMMuxNode:
         self.loop_rate = rospy.get_param("~loop_rate", 30.0)
 
         # ----- Mission3 타이밍 파라미터 -----
-        self.m3_phase_time          = rospy.get_param("~m3_phase_time", 36.0)   # 첫 LKAS cmd 이후 기준
-        self.m3_turn_duration       = rospy.get_param("~m3_turn_duration", 2.5)
-        self.m3_turn_speed          = rospy.get_param("~m3_turn_speed", 0.18)
-        self.m3_turn_yaw            = rospy.get_param("~m3_turn_yaw", -0.4)
+        self.m3_phase_time          = rospy.get_param("~m3_phase_time", 13.4)   # 첫 LKAS cmd 이후 기준
+        self.m3_turn_duration       = rospy.get_param("~m3_turn_duration", 0)
+        self.m3_turn_speed          = rospy.get_param("~m3_turn_speed", 0)
+        self.m3_turn_yaw            = rospy.get_param("~m3_turn_yaw", 0)
 
         # mission3 DWA 강제 구간
-        self.m3_force_obs_duration  = rospy.get_param("~m3_force_obs_duration", 6.0)
+        self.m3_force_obs_duration  = rospy.get_param("~m3_force_obs_duration", 0.0)
         # mission3 끝난 직후 base obstacle 강제 구간
-        self.m3_force_base_duration = rospy.get_param("~m3_force_base_duration", 7.0)
+        self.m3_force_base_duration = rospy.get_param("~m3_force_base_duration", 0.0)
 
         # ===== v2x 두 단계 타이머 =====
         # mission5: 첫 번째 v2x 턴이 시작되는 시점
-        self.mission5_phase_time = rospy.get_param("~mission5_phase_time", 1.5)
+        self.mission5_phase_time = rospy.get_param("~mission5_phase_time", 4)
         # v2x: 두 번째 턴이 시작되는 시점 (기존 v2x_phase_time 유지)
-        self.v2x_phase_time2    = rospy.get_param("~v2x_phase_time", 15.0)
+        self.v2x_phase_time2    = rospy.get_param("~v2x_phase_time", 6.5)
 
         # 두 턴에 *각각* 쓰는 턴 지속 시간
-        self.mission5_turn_duration = rospy.get_param("~mission5_turn_duration", 1)
-        self.v2x_turn_duration      = rospy.get_param("~v2x_turn_duration", 2.8)
+        self.mission5_turn_duration = rospy.get_param("~mission5_turn_duration", 0)
+        self.v2x_turn_duration      = rospy.get_param("~v2x_turn_duration", 1)
 
         # mission5(1단계 턴) 조향값
-        self.mission5_turn_speed = rospy.get_param("~mission5_turn_speed", 0.09)
-        self.mission5_turn_yaw   = rospy.get_param("~mission5_turn_yaw", -0.4)
+        self.mission5_turn_speed = rospy.get_param("~mission5_turn_speed", 0.0)
+        self.mission5_turn_yaw   = rospy.get_param("~mission5_turn_yaw", -0.00)
 
         # v2x(2단계 턴) 조향값 (기존 파라미터 유지)
-        self.v2x_turn_speed      = rospy.get_param("~v2x_turn_speed", 0.18)
-        self.v2x_turn_yaw        = rospy.get_param("~v2x_turn_yaw", 0.4)
+        self.v2x_turn_speed      = rospy.get_param("~v2x_turn_speed", 0.15)
+        self.v2x_turn_yaw        = rospy.get_param("~v2x_turn_yaw", -0.18)
 
         # --- 상태 변수들 ---
         self.current_mode = DriveMode.LANE_FOLLOW
